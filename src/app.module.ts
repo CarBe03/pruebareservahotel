8ac51830ec5a19155b5aa254c229ddc646611ca4
CarBe03 <carolopintag2003@hotmail.com>
2024-03-21 23:03:03 -0500

    habitaciones= mo, co, s

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HotelesModule } from './hoteles/hoteles.module';
+import { HabitacionesModule } from './habitaciones/habitaciones.module';
 
 @Module({
-  imports: [HotelesModule],
+  imports: [HotelesModule, HabitacionesModule],
   controllers: [AppController],
   providers: [AppService],
 })
