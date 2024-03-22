7cd925e930316bf02e1a1617539c55b5f0047b5a
CarBe03 <carolopintag2003@hotmail.com>
2024-03-22 18:39:17 -0500

    main

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './common/filters/http-exception.filter';
 import { TimeOutInterceptor } from './common/interceptors/timeout.interceptor';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionFilter());
   app.useGlobalInterceptors(new TimeOutInterceptor());
-  app.enableCors();
-
+  //app.enableCors();
+  const opciones = new DocumentBuilder()
+    .setTitle('Prueba Parcial N°2 APD')
+    .setDescription('Informe prueba Parcial N°2 APD')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, opciones);
+  SwaggerModule.setup('api/docs', app, document, {
+    swaggerOptions: {
+      filter: true,
+    },
+  });
 
   await app.listen(process.env.API_PORT || 3000);
 }
