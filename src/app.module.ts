bdceabafb5a89670b1637239b2fe1f1ca130c1a0
CarBe03 <carolopintag2003@hotmail.com>
2024-03-21 23:06:49 -0500

    clientes: mo, co, s

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,12 @@ import { HabitacionesModule } from './habitaciones/habitaciones.module';
 import { ReservasService } from './reservas/reservas.service';
 import { ReservasController } from './reservas/reservas.controller';
 import { ReservasModule } from './reservas/reservas.module';
+import { ClientesService } from './clientes/clientes.service';
+import { ClientesModule } from './clientes/clientes.module';
 
 @Module({
-  imports: [HotelesModule, HabitacionesModule, ReservasModule],
+  imports: [HotelesModule, HabitacionesModule, ReservasModule, ClientesModule],
   controllers: [AppController, ReservasController],
-  providers: [AppService, ReservasService],
+  providers: [AppService, ReservasService, ClientesService],
 })
 export class AppModule {}
